d4401c491c81a13e6f18deb8516266f67804b81e
ivanmerkush <ivan.merkushev@yandex.ru>
2019-10-22 21:32:05 +0300

    Initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
+import { ModalModule } from "ngx-bootstrap/modal";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
+import {Ng4LoadingSpinnerModule} from "ng4-loading-spinner";
+import {RouterModule, Routes} from "@angular/router";
+
+// const appRoutes: Routes = [
+//   {path: "", component: HomeComponent},
+//   {path: "home", component: HomeComponent},
+//   {path: "billing-details/:id", component: BillingDetailsViewComponent},
+//   {path: "**", component: NotFoundComponent}
+// ];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    Ng4LoadingSpinnerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    // RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
